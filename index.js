9b4d0ed8c5d7a7ca736900432e66e0e8c42b7e8b
Kevin Chang <keviny.chang@advantech.com>
2024-06-20 09:27:59 -0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function updatePointContent(element, title, content, link){
+    $(element).text(title)
+    $(element).append(content)// add content
+    element = element + ' > a'
+    $(element).attr('href', link)
+}
+
 var ti = "本周(6.17.2024 - 6.21.2024)晨兴与读经进度"
 $('title').text(ti)
 $('h1').text(ti)
@@ -8,9 +15,8 @@ $('.div1 > h3 > ul > .p1').text(book)
 ti = "进度："
 var content = "<a>第六周 基督作我们安息日的安息，由迦南美地所预表</a>"
 var link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2023:2023.07.%E5%86%AC%E5%AD%A3%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8"
-$('.div1 > h3 > ul > .p2').text(ti)
-$('.div1 > h3 > ul > .p2').append(content)// add content
-$('.div1 > h3 > ul > .p2 > a').attr('href', link)
+var elem = '.div1 > h3 > ul > .p2'
+updatePointContent(elem, ti, content, link)
 //Bible
 var oldTest = "民数记"
 var newTest = "罗马书"
@@ -18,42 +24,36 @@ $('.div2 > .sub').text("圣经")
 ti = "旧约：" + oldTest
 content = " <a>第4~8章</a>" //<------ update here every week
 link = "https://line.twgbr.org/recoveryversion/bible/04.html#C4Num"
-$('.div2 > h3 > ul > .p1').text(ti)
-$('.div2 > h3 > ul > .p1').append(content)// add content
-$('.div2 > h3 > ul > .p1 > a').attr('href', link)
+elem = '.div2 > h3 > ul > .p1'
+updatePointContent(elem, ti, content, link)
 ti = "新约：" + newTest
 content = " <a>第4~8章</a>" //<------ update here every week
 link = "https://line.twgbr.org/recoveryversion/bible/45.html#C4Rom"
-$('.div2 > h3 > ul > .p2').text(ti)
-$('.div2 > h3 > ul > .p2').append(content)// add content
-$('.div2 > h3 > ul > .p2 > a').attr('href', link)
+elem = '.div2 > h3 > ul > .p2'
+updatePointContent(elem, ti, content, link)
 ti = "认识基督系列认识基督系列："
 content = "<a>" + oldTest + "</a>"
 link = "https://substantial-baboon-a7f.notion.site/eecc41f80c404494b4faaec0088ea8b2"
-$('.div2 > h3 > ul > .p3').text(ti)
-$('.div2 > h3 > ul > .p3').append(content)// add content
-$('.div2 > h3 > ul > .p3 > a').attr('href', link)
+elem = '.div2 > h3 > ul > .p3'
+updatePointContent(elem, ti, content, link)
 ti = "认识基督系列认识基督系列："
 content = "<a>" + newTest + "</a>"
 link = "https://shard-desk-771.notion.site/8c64d024703f43b2a7ad8c8a665c9e64"
-$('.div2 > h3 > ul > .p4').text(ti)
-$('.div2 > h3 > ul > .p4').append(content)// add content
-$('.div2 > h3 > ul > .p4 > a').attr('href', link)
+elem = '.div2 > h3 > ul > .p4'
+updatePointContent(elem, ti, content, link)
 ti = "新约圣经导读："
 content = "<a>" + newTest + "</a>"
 link = "https://xinrenfuyin.org/?page_id=10686"
-$('.div2 > h3 > ul > .p5').text(ti)
-$('.div2 > h3 > ul > .p5').append(content)// add content
-$('.div2 > h3 > ul > .p5 > a').attr('href', link)
+elem = '.div2 > h3 > ul > .p5'
+updatePointContent(elem, ti, content, link)
 //Life Study
 newTest = "歌罗西书生命读经"
 $('.div3 > .sub').text("生命读经")
 ti = "进度："
 content = "<a>" + newTest + "</a>"
 link = "http://www.life-study1984.org/schedule/schedule.php?bookno=50-Y"
-$('.div3 > h3 > ul > .p1').text(ti)
-$('.div3 > h3 > ul > .p1').append(content)// add content
-$('.div3 > h3 > ul > .p1 > a').attr('href', link)
+elem = '.div3 > h3 > ul > .p1'
+updatePointContent(elem, ti, content, link)
 ti = "展览日期：8/15/2024"
 link = "http://www.life-study1984.org/schedule/schedule.php?bookno=50-Y"
 $('.div3 > h3 > ul > .p2').text(ti)
