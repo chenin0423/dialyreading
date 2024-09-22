7c44cac6f7a90d4c544e0b8172b2bd5963ae22ad
bobkkblue <chienyec@gmail.com>
2024-09-22 16:10:32 -0700

    update

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ function updatePointContent(element, title, content, link){
 
 const title = "本周(9.15.2024 - 9.21.2024)晨兴与读经进度"
 const morning_revival_book_title = "借着在基督耶稣里的恩典上得着加力，成为贵重的器皿，装备齐全的神人，好在神经纶的唯一职事中尽我们的职事 (2024年春季长老负责弟兄训练)"
-const morning_revival_this_week = "<a>第六周 作贵重的器皿，同那清心呼求主的人，竭力追求公义、信、爱、和平</a>"
-const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.02.%E6%98%A5%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AD%E5%91%A8"
-const youtube = `<br><iframe width="400" height="225" src="https://www.youtube.com/embed/w6ebX8q4EzE?si=UfFVnBm-WBtgV-su" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+const morning_revival_this_week = "<a>第七周 主与我们的灵同在，作我们加力的恩典，使我们能供应神永远经纶的健康教训，讲说生命之恩的健康话语，为着生机地建造召会作基督的身体</a>"
+const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.02.%E6%98%A5%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E4%B8%83%E5%91%A8"
+const youtube = `<br><iframe width="400" height="225" src="https://www.youtube.com/embed/ggvzcHRQiQM?si=tjC7XkNqnUJyTatw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
 
-const old_test1 = "申命记" 
-const old_test2 = "约书亚记" 
-const new_test1 = "帖撒罗尼迦前书"
-const new_test2 = "帖撒罗尼迦后书"
+const old_test1 = "约书亚记" 
+const new_test1 = "提摩太前书"
 
 var ti = title
 $('title').text(ti)
@@ -36,26 +34,15 @@ $(elem).append(youtube)
 $('.div2 > .sub').text("圣经")
 
 ti = "旧约：" + old_test1
-content = " <a>第33~34章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=5&f_ChapterNo=33"
+content = " <a>第4~8章</a>" //<------ update here every week
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=6&f_ChapterNo=4"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(1) > .p1'
 updatePointContent(elem, ti, content, link)
-ti = "旧约：" + old_test2
-content = " <a>第1~3章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=6&f_ChapterNo=1"
-elem = 'body > div.div2 > h3 > ul > li:nth-child(1) > .p2'
-updatePointContent(elem, ti, content, link)
 
-var newTest = "帖撒罗尼迦前书"
 ti = "新约：" + new_test1
-content = " <a>第4~5章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=52&f_ChapterNo=4"
+content = " <a>第1~5章</a>" //<------ update here every week
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=54&f_ChapterNo=1"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p1'
-updatePointContent(elem, ti, content, link)
-ti = "新约：" + new_test2
-content = " <a>第1~3章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=53&f_ChapterNo=1"
-elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p2'
 updatePointContent(elem, ti, content, link)
 
 
