3e6b519fb913d86a72cb414a48120c5226d7e84c
bobkkblue <chienyec@gmail.com>
2024-10-06 19:52:59 -0700

    update

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function updatePointContent(element, title, content, link){
     $(element).attr('href', link)
 }
 
-const title = "本周(9.29.2024 - 10.05.2024)晨兴与读经进度"
-const morning_revival_book_title = "借着在基督耶稣里的恩典上得着加力，成为贵重的器皿，装备齐全的神人，好在神经纶的唯一职事中尽我们的职事 (2024年春季长老负责弟兄训练)"
-const morning_revival_this_week = `<a>第八周 “尽你的职事”</a>`
-const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.02.%E6%98%A5%E5%AD%A3%E9%95%BF%E8%80%81%E8%B4%9F%E8%B4%A3%E5%BC%9F%E5%85%84%E8%AE%AD%E7%BB%83:%E7%AC%AC%E5%85%AB%E5%91%A8"
-const youtube = `<br><iframe width="400" height="225" src="https://www.youtube.com/embed/fh5XG-wixbg?si=PErNy6wFujvzzRyO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+const title = "本周(10.06.2024 - 10.12.2024)晨兴与读经进度"
+const morning_revival_book_title = "2024年国殇节特会 基督徒的生活"
+const morning_revival_this_week = `<a>第一周 基督徒生活的内在意义</a>`
+const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.03.%E5%9B%BD%E6%AE%87%E8%8A%82%E7%89%B9%E4%BC%9A:%E7%AC%AC%E4%B8%80%E5%91%A8"
+const youtube = `<br><iframe width="400" height="225" src="https://www.youtube.com/embed/5AqHYq4xYWY?si=RPw47KvNf43ys82e" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
 
 const old_test1 = "约书亚记" 
 const new_test1 = "提摩太前书"
@@ -36,8 +36,8 @@ $(elem).append(youtube)
 $('.div2 > .sub').text("圣经")
 
 ti = "旧约：" + old_test1
-content = " <a>第9~13章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=6&f_ChapterNo=9"
+content = " <a>第14~18章</a>" //<------ update here every week
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=6&f_ChapterNo=14"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(1) > .p1'
 updatePointContent(elem, ti, content, link)
 
