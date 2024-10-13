e17a9c86ae73425aa909ccc4adc10bc764f94db2
bobkkblue <chienyec@gmail.com>
2024-10-13 13:18:44 -0700

    weekly update

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ function updatePointContent(element, title, content, link){
 
 const title = "本周(10.06.2024 - 10.12.2024)晨兴与读经进度"
 const morning_revival_book_title = "2024年国殇节特会 基督徒的生活"
-const morning_revival_this_week = `<a>第一周 基督徒生活的内在意义</a>`
-const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.03.%E5%9B%BD%E6%AE%87%E8%8A%82%E7%89%B9%E4%BC%9A:%E7%AC%AC%E4%B8%80%E5%91%A8"
-const youtube = `<br><iframe width="400" height="225" src="https://www.youtube.com/embed/5AqHYq4xYWY?si=RPw47KvNf43ys82e" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+const morning_revival_this_week = `<a>第二周 接枝的生命</a>`
+const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.03.%E5%9B%BD%E6%AE%87%E8%8A%82%E7%89%B9%E4%BC%9A:%E7%AC%AC%E4%BA%8C%E5%91%A8"
+const youtube = `<br><iframe width="400" height="225" src="https://www.youtube.com/embed/YKEQfwRU0WQ?si=JFtJ8U4B_sl7fD6c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
 
 const old_test1 = "约书亚记" 
-const new_test1 = "提多书"
-const new_test2 = "腓利门书"
-const new_test3 = "希伯来书"
+const new_test1 = "希伯来书"
 
 
 var ti = title
@@ -37,29 +35,16 @@ $(elem).append(youtube)
 $('.div2 > .sub').text("圣经")
 
 ti = "旧约：" + old_test1
-content = " <a>第14~18章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=6&f_ChapterNo=14"
+content = " <a>第19~23章</a>" //<------ update here every week
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=6&f_ChapterNo=19"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(1) > .p1'
 updatePointContent(elem, ti, content, link)
 
 ti = "新约：" + new_test1
-content = " <a>第1~3章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=56&f_ChapterNo=1"
+content = " <a>第2~6章</a>" //<------ update here every week
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=58&f_ChapterNo=2"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p1'
 updatePointContent(elem, ti, content, link)
-
-ti = "新约：" + new_test2
-content = " <a>第1章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=57&f_ChapterNo=1"
-elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p2'
-updatePointContent(elem, ti, content, link)
-
-ti = "新约：" + new_test3
-content = " <a>第1章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=58&f_ChapterNo=1"
-elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p3'
-updatePointContent(elem, ti, content, link)
-
 
 //Life Study
 newTest = "歌罗西书生命读经"
