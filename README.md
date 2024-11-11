30d39e9d75cd8940dd3761a6550abf40f32bbd33
bobkkblue <chienyec@gmail.com>
2024-11-10 22:08:16 -0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 mkdir ./prev/09082024
 cp index.html ./prev/09082024
 cp index.js ./prev/09082024
-
--- index.js
+++ index.js
@@ -5,15 +5,18 @@ function updatePointContent(element, title, content, link){
     $(element).attr('href', link)
 }
 
-const title = "本周(11.03.2024 - 11.09.2024)晨兴与读经进度"
+const title = "本周(11.10.2024 - 11.16.2024)晨兴与读经进度"
 const morning_revival_book_title = "2024年国殇节特会 基督徒的生活"
-const morning_revival_this_week = `<a>第五周 享受生命的流带着生命的供应，出于并为着神宏伟的殿，借此过基督徒的生活</a>`
-const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.03.%E5%9B%BD%E6%AE%87%E8%8A%82%E7%89%B9%E4%BC%9A:%E7%AC%AC%E4%BA%94%E5%91%A8"
-const youtube = `<br><iframe width="350" height="197" src="https://www.youtube.com/embed/nD20JoqW0IE?si=IJ3Bpg1Y-uA1xpmO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+const morning_revival_this_week = `<a>第六周 三一神在基督里借着照在我们心里而作我们的生命</a>`
+const morning_revival_link = "https://stemofjesse.org/doku/doku.php/%E6%99%A8%E5%85%B4%E5%9C%A3%E8%A8%80:2024:2024.03.%E5%9B%BD%E6%AE%87%E8%8A%82%E7%89%B9%E4%BC%9A:%E7%AC%AC%E5%85%AD%E5%91%A8"
+const youtubeSrc = "https://www.youtube.com/embed/Ywj0NOX-RTY?si=1NGT__cWvh8Wu0Km"
+const youtube = `<br><iframe width="350" height="197" src=${youtubeSrc} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
  
+
+
 const old_test1 = "士师记" 
-const new_test1 = "雅各书"
-const new_test2 = "彼得前书"
+const new_test1 = "彼得前书"
+const new_test2 = "彼得后书"
 
 
 var ti = title
@@ -36,20 +39,20 @@ $(elem).append(youtube)
 $('.div2 > .sub').text("圣经")
 
 ti = "旧约：" + old_test1
-content = " <a>第10~14章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=7&f_ChapterNo=10"
+content = " <a>第15~19章</a>" //<------ update here every week
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=7&f_ChapterNo=15"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(1) > .p1'
 updatePointContent(elem, ti, content, link)
 
 ti = "新约：" + new_test1
 content = " <a>第4~5章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=59&f_ChapterNo=4"
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=60&f_ChapterNo=4"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p1'
 updatePointContent(elem, ti, content, link)
 
 ti = "新约：" + new_test2
 content = " <a>第1~3章</a>" //<------ update here every week
-link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=60&f_ChapterNo=1"
+link = "https://www.recoveryversion.com.tw/Style0A/026/read_List.php?f_BookNo=61&f_ChapterNo=1"
 elem = 'body > div.div2 > h3 > ul > li:nth-child(2) > .p2'
 updatePointContent(elem, ti, content, link)
 
